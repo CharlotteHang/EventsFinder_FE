96a8d372cb47a8bf97d3ff640358d95cae33a942
Your Name <you@example.com>
2020-11-23 02:11:27 -0800

    debug invalid email

--- src/types/Recommendation.ts
+++ src/types/Recommendation.ts
@@ -1,26 +1,26 @@
 export interface Recommendation {
-    itemId: string;
-    name: string;
-    rating: number;
-    address: string;
-    categories:Category[];
-    imageUrl: string;
-    url: string;
-    distance:number;
-    favorite:boolean;
+  itemId: string;
+  name: string;
+  rating: number; // rank
+  address: string;
+  categories: Category[];
+  imageUrl: string;
+  url: string;
+  distance: number;
+  favorite: boolean;
 
-    localDate: string;
-    time: string;
-    type: string;
-    status: string;
-  }
+  localDate: string;
+  time: string;
+  type: string;
+  status: string;
+}
 
-  export interface RecommendationResponse {
-    data: Recommendation[];
-    errors: string[];
-    missingFields:string[];
-  }
+export interface RecommendationResponse {
+  data: Recommendation[];
+  errors: string[];
+  missingFields: string[];
+}
 
-  export interface Category {
-    categoryType: string
-  }
+export interface Category {
+  categoryType: string;
+}
